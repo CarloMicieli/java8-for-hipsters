a72741f07e9d2b3b5c18121640e220ad5426c1f4
CarloMicieli <carlo.micieli@gmail.com>
2014-03-05 21:23:43 +0100

    Add tests

--- src/test/java/io/github/carlomicieli/java8/FunctionalInterfaceTests.java
+++ src/test/java/io/github/carlomicieli/java8/FunctionalInterfaceTests.java
@@ -15,8 +15,47 @@
  */
 package io.github.carlomicieli.java8;
 
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
 /**
  * @author Carlo Micieli
  */
 public class FunctionalInterfaceTests {
+
+    @Test
+    public void shouldDefinePredicateAsInterfaces() {
+        MyPredicate<String> p = s -> s.length() > 5;
+        assertThat(TestClass.applyPredicate(p, "123456"), is(true));
+        assertThat(TestClass.applyPredicate(p, "1234"), is(false));
+    }
+
+    @Test
+    public void shouldDefineFunctionsAsInterfaces() {
+        MyFunction<String, String, Integer> f = (s1, s2) -> s1.length() + s2.length();
+        assertThat(TestClass.applyFunction(f, "abc", "def"), is(equalTo(6)));
+    }
+
+    @FunctionalInterface
+    private static interface MyPredicate<T> {
+        boolean apply(T t);
+    }
+
+    @FunctionalInterface
+    private static interface MyFunction<A, B, R> {
+        R apply(A a, B b);
+    }
+
+    private static class TestClass {
+        public static <T> boolean applyPredicate(MyPredicate<T> p, T t) {
+            return p.apply(t);
+        }
+
+        public static int applyFunction(MyFunction<String, String, Integer> fun, String a, String b) {
+            return fun.apply(a, b);
+        }
+    }
 }
