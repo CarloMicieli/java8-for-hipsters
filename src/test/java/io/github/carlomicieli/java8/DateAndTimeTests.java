6c902948953bdb094d4b25da1856faec95ea3f1c
CarloMicieli <carlo.micieli@gmail.com>
2014-03-08 22:01:01 +0100

    Added date/time tests.

--- src/test/java/io/github/carlomicieli/java8/DateAndTimeTests.java
+++ src/test/java/io/github/carlomicieli/java8/DateAndTimeTests.java
@@ -20,7 +20,10 @@ import org.junit.Test;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.Month;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
+import static java.time.format.DateTimeFormatter.ofPattern;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
@@ -29,6 +32,9 @@ import static org.junit.Assert.assertThat;
  * @author Carlo Micieli
  */
 public class DateAndTimeTests {
+
+    private final LocalDate SEPTEMBER_9TH = LocalDate.of(1981, 9, 9);
+
     @Test
     public void shouldObtainLocalDateFromYearMonthAndDay() {
         LocalDate september9th = LocalDate.of(1981, 9, 9);
@@ -40,8 +46,22 @@ public class DateAndTimeTests {
     @Test
     public void shouldObtainLocalDateFromYearAndDayOfYear() {
         LocalDate september9th = LocalDate.ofYearDay(1981, 252);
-        assertThat(september9th, is(equalTo(LocalDate.of(1981, Month.SEPTEMBER, 9))));
+        assertThat(september9th, is(equalTo(SEPTEMBER_9TH)));
         assertThat(september9th.getDayOfYear(), is(equalTo(252)));
+    }
+
+    @Test
+    public void shouldFormatLocalDateValues() {
+        DateTimeFormatter dtf = ofPattern("YYYY-MM-dd").withLocale(Locale.US);
+        String dateAsString = SEPTEMBER_9TH.format(dtf);
+        assertThat(dateAsString, is(equalTo("1981-09-09")));
+        assertThat(SEPTEMBER_9TH.format(DateTimeFormatter.BASIC_ISO_DATE), is("19810909"));
+    }
+
+    @Test
+    public void shouldParseStringAsLocalDate() {
+        LocalDate date = LocalDate.parse("1981-09-09");
+        assertThat(date, is(equalTo(SEPTEMBER_9TH)));
     }
 
     @Test
